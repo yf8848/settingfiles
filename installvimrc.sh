4694ad4451a9bdc64ebc64f1597b02be4b1c622e
Oozwding <zwdingcoder@gmail.com>
2016-05-17 20:52:07 +0800

    installvimrc.sh

--- installvimrc.sh
+++ installvimrc.sh
@@ -1,2 +1,43 @@
 #!/bin/bash
-echo "bash file test"
+#!/bin/bash
+
+cd ~
+git clone https://github.com/Oozwding/settingfiles.git
+cd ~
+
+if [ -e oldvim ]
+then
+	rm -Rf oldvim
+fi
+
+if [ -e .vim ]
+then
+	#if .vim exist, check if .vimrc exists
+	if [ -e .vimrc ]
+	#if both exist, create a dir and move both
+	then
+		mkdir -p oldvim
+		mv .vim oldvim/.vim
+		mv .vimrc oldvim/.vimrc
+	#if only .vim exists
+	else
+		mkdir -p oldvim
+		mv .vim oldvim/.vim
+	fi
+else
+	#if only .vimrc exists
+	if [ -e .vimrc ]
+	then
+		mkdir -p oldvim
+		mv .vimrc oldvim/.vimrc
+	fi
+fi
+
+cd
+cd settingfiles
+mv -f .vimrc ..
+cd ..
+rm -Rf settingfiles
+mkdir -p .vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo '\n' | vim +PluginInstall +qall
